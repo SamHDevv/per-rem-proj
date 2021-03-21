75c062ab529919bebe1cf219f5c4a3d3ff72e75d
Samuel <74045391+SamHnnz@users.noreply.github.com>
2021-03-21 15:06:55 +0100

    Experience Component Completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,7 +26,7 @@ const Navbar = () => {
               <a className="nav-link" href="#">Services</a>
             </li>           
             <li className="nav-item">
-              <a className="nav-link" href="#">How work</a>
+              <a className="nav-link" href="#">Experience</a>
             </li>           
             <li className="nav-item">
               <a className="nav-link" href="#">Portfolio</a>
