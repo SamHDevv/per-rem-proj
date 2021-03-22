b03db882d0435695ee29efbde89db55127ffb2cc
Samuel <74045391+SamHnnz@users.noreply.github.com>
2021-03-22 21:55:21 +0100

    contact-form v1

---
+++ src/components/Contacts.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Contacts = () => {
+    return (
+        <div className="contacts">
+            <h1>Contact me</h1>
+            <p>Dolor duis esse culpa Lorem cillum. Aute qui id ullamco non consectetur mollit. Sint ad tempor voluptate in sint ad exercitation. Sunt anim mollit esse occaecat est labore dolor tempor est do pariatur esse occaecat. Cupidatat aliqua dolore cupidatat qui proident. Duis non non cillum ullamco.</p>
+            
+        </div>
+    )
+}
+
+export default Contacts
